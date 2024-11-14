b916bb71d25d5ce7e1139c9ef4431c52a603d969
Earts-vinay <vinay.r@entitledarts.com>
2024-11-14 10:28:59 +0530

    first

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
